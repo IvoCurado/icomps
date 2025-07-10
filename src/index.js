ec60d893b33c14c13c7f90e713b826d636ea745e
Ivo Curado <ivocurado@live.com.pt>
2025-07-10 22:16:20 +0100

    feat: Add base to carousel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import "./styles.css";
-import { initializeDropDowns } from "./modules/dropdown";
+import { addCarouselBehavior } from "./modules/carousel";
 
-function main() {
-  initializeDropDowns();
+export function configMenuDropDown(buttonElement, dropdownElement) {
+  if (!buttonElement._listenerAdded) {
+    buttonElement.addEventListener("click", (event) => {
+      dropdownElement.classList.toggle("hidden");
+    });
+    buttonElement._listenerAdded = true;
+  }
 }
 
+function main() {
+  const menuButtonElement = document.querySelector(".menu-button");
+  const menuDropDownElement = document.querySelector(".menu-options-zone");
+  configMenuDropDown(menuButtonElement, menuDropDownElement);
+  
+  addCarouselBehavior();
+}
 main();
+
