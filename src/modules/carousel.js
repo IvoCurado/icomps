cff0bc9a0624d5f115c3d4a63c8c6e6e681b13f4
Ivo Curado <ivocurado@live.com.pt>
2025-07-11 18:52:03 +0100

    feat: Add carousel without nav

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,4 +1,23 @@
+export function addCarouselBehavior() {
+  let currentYTranslation = 0;
+  const carouselImagesZone = document.querySelector(".carousel-images-zone");
 
-export function addCarouselBehavior(){
-
+  const previousButton = document.getElementById("previous-button");
+  previousButton.addEventListener("click", (event) => {
+    if (currentYTranslation === 0) {
+      currentYTranslation = -5;
+    } else {
+      currentYTranslation += 1;
+    }
+    carouselImagesZone.style.transform = `translate(${currentYTranslation * 110}%, 0px)`;
+  });
+  const nextButton = document.getElementById("next-button");
+  nextButton.addEventListener("click", (event) => {
+    if (currentYTranslation === -5) {
+      currentYTranslation = 0;
+    } else {
+      currentYTranslation -= 1;
+    }
+    carouselImagesZone.style.transform = `translate(${currentYTranslation * 110}% , 0px)`;
+  });
 }
